0bd23c62be30a5464a4b0130a5aacea342f33bf8
selman adanir <adanirselman@gmail.com>
2022-07-25 01:14:05 +0300

    create label and view and make with dark mode appearance

--- DarkMode/ViewController.swift
+++ DarkMode/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let myView: UIView = {
+        let view = UIView(frame: CGRect(x: 20, y: 100, width: 200, height: 100))
+        view.backgroundColor = .secondarySystemBackground
+        return view
+    }()
+    
+    private let myLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.text = "Hello, world!"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        myLabel.frame = myView.bounds
+        myView.addSubview(myLabel)
+        
+        
+        view.addSubview(myView)
+        
+        view.backgroundColor = .systemBlue
+        
+        switch traitCollection.userInterfaceStyle {
+        case .dark:
+            break
+        case .light:
+            break
+        default:
+            print("Something les")
+        }
     }
-
-
 }
-
